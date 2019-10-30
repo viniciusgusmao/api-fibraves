29493cea04e7391bcfc766b669c670ef7c77ddee
Vinicius <vinicius-og@hotmail.com>
2019-10-29 21:06:20 -0300

    first commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'userfibraves',
+  password: 'userfibraves',
+  database: 'fibraves',
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+}
